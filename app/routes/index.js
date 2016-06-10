bd2353e0fa8d083e3ca10a03be1ee2f960f7ce61
Scott Holland <scotthollandjr@gmail.com>
2016-06-10 12:22:03 -0700

    Add functionality for deleting comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('comment');
   },
+
+  actions: {
+    save3(params) {
+      var newComment = this.store.createRecord('comment', params);
+      newComment.save();
+      this.transitionTo('index');
+    },
+    destroyComment(comment) {
+      comment.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
