5f467b848bf902449af937bc719e28ba929971e3
Scott Holland <scotthollandjr@gmail.com>
2016-06-17 17:38:50 -0700

    Add more styling changes

--- app/helpers/answered.js
+++ app/helpers/answered.js
@@ -4,11 +4,11 @@ export function answered(params) {
   var question = params[0];
 
   if (question.get('comments').get('length') === 0) {
-    return Ember.String.htmlSafe('<span class="unanswered">(no answers yet!)</span>');
+    return Ember.String.htmlSafe('<span>[unanswered]</span>');
   } else if (question.get('comments').get('length') === 1) {
-    return Ember.String.htmlSafe('<span class="answerSpan">(' + question.get('comments').get('length') + ' answer)</span>');
+    return Ember.String.htmlSafe('<span class="answerSpan">[' + question.get('comments').get('length') + ' answer]</span>');
   } else {
-    return Ember.String.htmlSafe('<span class="answerSpan">(' + question.get('comments').get('length') + ' answers)</span>');
+    return Ember.String.htmlSafe('<span class="answerSpan">[' + question.get('comments').get('length') + ' answers]</span>');
   }
 }
 
